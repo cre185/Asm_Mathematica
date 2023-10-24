64ce8304366ca61da22911e054587dcb15acb09d
cre185 <2907692855@qq.com>
2023-10-24 23:08:16 +0800

    update 2023.10.24

--- Asm_Mathematica/main.asm
+++ Asm_Mathematica/main.asm
@@ -1,15 +1,3 @@
-TITLE Windows Application                   (WinApp_v2.asm)
-
-; Another version of WinApp.asm
-; Modified by: HenryFox
-; Last update: 10/13/21
-; Original version uses Irvine32 and GraphWin, this version uses windows.inc
-
-; This program displays a resizable application window and
-; several popup message boxes.
-; Thanks to Tom Joyce for creating a prototype
-; from which this program was derived.
-
 .386
 .model flat, stdcall
 option casemap: none
@@ -30,21 +18,6 @@ includelib      shell32.lib
 include  		calculate.inc
 include			resource.inc
 
-;------------------ Structures ----------------
-
-WNDCLASS STRUC
-  style           DWORD ?
-  lpfnWndProc     DWORD ?
-  cbClsExtra      DWORD ?
-  cbWndExtra      DWORD ?
-  hInstance       DWORD ?
-  hIcon           DWORD ?
-  hCursor         DWORD ?
-  hbrBackground   DWORD ?
-  lpszMenuName    DWORD ?
-  lpszClassName   DWORD ?
-WNDCLASS ENDS
-
 MSGStruct STRUCT
   msgWnd        DWORD ?
   msgMessage    DWORD ?
@@ -59,6 +32,7 @@ MAIN_WINDOW_STYLE = WS_VISIBLE+WS_DLGFRAME+WS_CAPTION+WS_BORDER+WS_SYSMENU \
 
 ;==================== DATA =======================
 .data
+FromEdit DWORD 10000h
 
 AppLoadMsgTitle BYTE "Application Loaded",0
 AppLoadMsgText  BYTE "This window displays when the WM_CREATE "
@@ -72,22 +46,27 @@ GreetTitle BYTE "Main Window Active",0
 GreetText  BYTE "This window is shown immediately after "
 	       BYTE "CreateWindow and UpdateWindow are called.",0
 
-CloseMsg   BYTE "WM_CLOSE message received",0
-
 ErrorTitle  BYTE "Error",0
+EmptyText BYTE 0
+public EmptyText
+
 WindowName  BYTE "Asm Mathematica",0
 className   BYTE "ASMWin",0
 
 fileMsg    BYTE "文件",0
 subMsg     BYTE "新建",0
 sub2ndMsg  BYTE "打开",0
+EDIT	   BYTE "Edit",0
 
 msg	      MSGStruct <>
 winRect   RECT <>
-hMainWnd  DWORD ?
+hMainWnd  DWORD 0
+hEditWnd  DWORD ?
+public hMainWnd, hEditWnd
 
 hFileMenu DWORD ?
 hSubMenu  DWORD ?
+public hFileMenu, hSubMenu
 
 hInstance DWORD ?
 
@@ -97,6 +76,9 @@ MainWin WNDCLASS <NULL,WinProc,NULL,NULL,NULL,NULL,NULL, \
 
 ;=================== CODE =========================
 .code
+
+InitMenu PROTO 
+
 WinMain PROC
 ; Get a handle to the current process.
 	INVOKE GetModuleHandle, NULL
@@ -109,8 +91,12 @@ WinMain PROC
 	INVOKE LoadCursor, NULL, IDC_ARROW
 	mov MainWin.hCursor, eax
 
+; Initialize brush for main window 
+	INVOKE CreateSolidBrush, 0ffffffh
+	mov MainWin.hbrBackground, eax
+
 ; Register the window class.
-	INVOKE RegisterClass, ADDR MainWin
+	INVOKE RegisterClassA, ADDR MainWin
 	.IF eax == 0
 	  call ErrorHandler
 	  jmp Exit_Program
@@ -131,22 +117,11 @@ WinMain PROC
 	.ENDIF
 
 ; Create a menu
-	INVOKE CreateMenu
-	mov hFileMenu, eax
-	INVOKE CreateMenu
-	mov hSubMenu, eax
-	INVOKE AppendMenuA, hSubMenu, 0, NULL, ADDR subMsg
-	INVOKE AppendMenuA, hSubMenu, 0, NULL, ADDR sub2ndMsg
-	INVOKE AppendMenuA, hFileMenu, 10h, hSubMenu, ADDR fileMsg
-	INVOKE SetMenu, hMainWnd, hFileMenu
+	INVOKE InitMenu
 
 ; Show and draw the window.
 	INVOKE ShowWindow, hMainWnd, SW_SHOW
 	INVOKE UpdateWindow, hMainWnd
-
-; Display a greeting message.
-	INVOKE MessageBox, hMainWnd, ADDR GreetText,
-	  ADDR GreetTitle, MB_OK
 
 ; Begin the program's message-handling loop.
 Message_Loop:
@@ -155,8 +130,11 @@ Message_Loop:
 
 	; Quit if no more messages.
 	.IF eax == 0
-	  jmp Exit_Program
+		jmp Exit_Program
 	.ENDIF
+
+	; Translate the message for edit to receive it 
+	INVOKE TranslateMessage, ADDR msg
 
 	; Relay the message to the program's WinProc.
 	INVOKE DispatchMessage, ADDR msg
@@ -165,6 +143,62 @@ Message_Loop:
 Exit_Program:
 	  INVOKE ExitProcess,0
 WinMain ENDP
+
+;-----------------------------------------------------
+InitMenu PROC
+; Initialize the menu
+;-----------------------------------------------------
+	INVOKE CreateMenu
+	mov hFileMenu, eax
+	INVOKE CreateMenu
+	mov hSubMenu, eax
+	INVOKE AppendMenuA, hSubMenu, 0, NULL, ADDR subMsg
+	INVOKE AppendMenuA, hSubMenu, 0, NULL, ADDR sub2ndMsg
+	INVOKE AppendMenuA, hFileMenu, 10h, hSubMenu, ADDR fileMsg
+	INVOKE SetMenu, hMainWnd, hFileMenu
+InitMenu ENDP
+
+;-----------------------------------------------------
+EditSize PROC
+; Change the size of edit based on the window size
+;-----------------------------------------------------
+.data 
+	WndRect RECT <>
+
+.code
+	.IF hMainWnd == 0 
+		ret
+	.ENDIF
+	push eax
+	push ebx
+	mov eax, hMainWnd
+	mov eax, hEditWnd
+	INVOKE GetWindowRect, hMainWnd, ADDR WndRect
+	.IF eax == 0
+		 call ErrorHandler
+	.ENDIF
+	mov eax, WndRect.right
+	sub eax, WndRect.left
+	mov ebx, WndRect.bottom
+	sub ebx, WndRect.top
+	sub eax, 100
+	sub ebx, 120
+	INVOKE MoveWindow, hEditWnd, 30, 30, eax, ebx, 1
+	pop ebx
+	pop eax
+	ret
+EditSize ENDP
+
+;-----------------------------------------------------
+EditProc PROC,
+	hWnd:DWORD, localMsg:DWORD, wParam:DWORD, lParam:DWORD
+; The edit box's message handler, which handles
+; application-specific messages. All other messages
+; are forwarded to the default Windows message
+; handler.
+;-----------------------------------------------------
+	ret
+EditProc ENDP
 
 ;-----------------------------------------------------
 WinProc PROC,
@@ -176,22 +210,31 @@ WinProc PROC,
 ;-----------------------------------------------------
 	mov eax, localMsg
 
-	.IF eax == WM_LBUTTONDOWN		; mouse button?
-	  INVOKE MessageBox, hWnd, ADDR PopupText,
-	    ADDR PopupTitle, MB_OK
-	  jmp WinProcExit
-	.ELSEIF eax == WM_CREATE		; create window?
-	  INVOKE MessageBox, hWnd, ADDR AppLoadMsgText,
-	    ADDR AppLoadMsgTitle, MB_OK
-	  jmp WinProcExit
-	.ELSEIF eax == WM_CLOSE		; close window?
-	  INVOKE MessageBox, hWnd, ADDR CloseMsg,
-	    ADDR WindowName, MB_OK
-	  INVOKE PostQuitMessage,0
-	  jmp WinProcExit
+	.IF eax == WM_CREATE		; create window
+		INVOKE MessageBox, hWnd, ADDR AppLoadMsgText,
+			ADDR AppLoadMsgTitle, MB_OK
+
+		; The edit box
+		INVOKE CreateWindowExA, 0, ADDR EDIT,
+			ADDR EDIT, WS_CHILD+WS_VISIBLE+ES_LEFT+ES_MULTILINE, 
+			CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 
+			CW_USEDEFAULT,hWnd,FromEdit,hInstance,NULL
+		.IF eax == 0
+			call ErrorHandler
+		.ENDIF
+		mov hEditWnd, eax
+		INVOKE ShowWindow, hEditWnd, SW_SHOW
+		INVOKE SendMessage, hEditWnd, WM_SETTEXT, 0, ADDR EmptyText
+
+		jmp WinProcExit
+	.ELSEIF eax == WM_CLOSE		; close window
+		INVOKE PostQuitMessage,0
+		jmp WinProcExit
+	.ELSEIF eax == WM_SIZE
+		INVOKE EditSize
 	.ELSE		; other message?
-	  INVOKE DefWindowProc, hWnd, localMsg, wParam, lParam
-	  jmp WinProcExit
+		INVOKE DefWindowProc, hWnd, localMsg, wParam, lParam
+		jmp WinProcExit
 	.ENDIF
 
 WinProcExit:
@@ -215,12 +258,14 @@ messageID  DWORD ?
 	  ADDR pErrorMsg,NULL,NULL
 
 	; Display the error message.
-	INVOKE MessageBox,NULL, pErrorMsg, ADDR ErrorTitle,
+	INVOKE MessageBox, NULL, pErrorMsg, ADDR ErrorTitle,
 	  MB_ICONERROR+MB_OK
 
 	; Free the error message string.
 	INVOKE LocalFree, pErrorMsg
 	ret
 ErrorHandler ENDP
+
+; INVOKE SendMessage, hEditWnd, WM_SETTEXT, 0, ADDR TestText
 
 END WinMain
