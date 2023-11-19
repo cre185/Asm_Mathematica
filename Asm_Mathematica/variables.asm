5ff65082cfa7fb4078a6247abdfaa01e23844bd4
者霖 陈 <2824964418@qq.com>
2023-11-19 21:44:59 +0800

    var frame

---
+++ Asm_Mathematica/variables.asm
@@ -0,0 +1,19 @@
+.386
+.model flat, stdcall
+option casemap: none
+
+include  		calculate.inc
+include			macro.inc
+strncpy			PROTO C :ptr sbyte, :ptr sbyte, :DWORD
+strcpy			PROTO C :ptr sbyte, :ptr sbyte
+strcat			PROTO C :ptr sbyte, :ptr sbyte
+memset			PROTO C :ptr sbyte, :DWORD, :DWORD
+strlen			PROTO C :ptr sbyte
+
+.data
+
+
+.code
+
+
+END
